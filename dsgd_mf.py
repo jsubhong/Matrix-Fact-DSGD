77c95657b4f71b6de0807b7727434b383b178272
Jinsub <jsubhong@gmail.com>
2015-04-17 17:23:05 -0400

    code

--- dsgd_mf.py
+++ dsgd_mf.py
@@ -2,14 +2,12 @@
 import os
 import sys
 import numpy as np
-from scipy.sparse import csr_matrix
-import random
 
 # Path for spark source folder
-os.environ['SPARK_HOME'] = "/Users/jinsub/Desktop/ML Big Datasets/spark-1.3.0-bin-cdh4"
+os.environ['SPARK_HOME'] = "/Users/jinsub/Desktop/ML_Big_Datasets/spark-1.3.0-bin-cdh4"
 
 # Append pyspark  to Python Path
-sys.path.append("/Users/jinsub/Desktop/ML Big Datasets/spark-1.3.0-bin-cdh4/python")
+sys.path.append("/Users/jinsub/Desktop/ML_Big_Datasets/spark-1.3.0-bin-cdh4/python")
 
 from pyspark import SparkContext
 
@@ -34,24 +32,20 @@ def initialiseWH(Vrdd):
     numCols = int(colValues.max())
 
     # create factor matrix W with row number as key
-    noIndexW = np.random.uniform(0.1, 10, (numRows, numFactors))
-    # print noIndexW
+    noIndexW = np.random.uniform(0.1, 10, (500, numFactors))
+
     indexW = [(x, ("W", noIndexW[x])) for x in range(len(noIndexW))]
     del noIndexW
     initialWrdd = sc.parallelize(indexW)
     del indexW
 
     # create factor matrix H with col number as key
-    noIndexH = np.random.uniform(0.1, 10, (numCols, numFactors))
+    noIndexH = np.random.uniform(0.1, 10, (500, numFactors))
     indexH = [(x, ("H", noIndexH[x])) for x in range(len(noIndexH))]
     del noIndexH
     initialHrdd = sc.parallelize(indexH)
     del indexH
 
-    # # initialHrdd = sc.parallelize(np.random.uniform(0.1, 10, (numFactors, numCols)))
-    # # initialHrdd = initialHrdd.keyBy(lambda x: x[1])
-    # print initialWrdd.collect()
-    # print initialWrdd.collect()
     return initialWrdd, initialHrdd
 
 #parser used to read directory of netflix files - put to triple format
@@ -96,12 +90,6 @@ def sgd(iterator):
                          LAMBDA/len(Vij) * Hj[dataIndex % len(Hj)]))
 
     return WUpdates, HUpdates
-            # return [triple]
-    # triples = contentTuple[1]
-    # for triple in triples:
-    #     print triple
-
-    # return ([(row,np.array),()],[(col,np.array),()])
 
 #Loss function
 def calcLoss(iterator):
@@ -136,20 +124,9 @@ def performUpdates(numIterations, V, W, H):
             H = Horiginal.map(lambda x: (x[0] % numWorkers, x[1]))
             currStratum = V.filter(lambda x: (abs(int(x[0]) - int(x[1][1])) % numWorkers) == stratumNumber)
             grouped = currStratum.groupWith(W, H)
-            # print grouped.collect()
-            # print map((lambda (x,y): (x, (list(y[0]), list(y[1]), list(y[2])))),                 sorted(list(grouped.collect())))
             partitioned = grouped.partitionBy(numWorkers, lambda x: x)
-            # print partitioned.collect()
-            # print map((lambda (x, y): (x, (list(y[0]), list(y[1]), list(y[2])))),                 sorted(list(partitioned.collect())))
-            # print partitioned.collect()
             updates = partitioned.mapPartitions(sgd)
-            loss = partitioned.mapPartitions(calcLoss)
 
-            # Wfile.write(str(sum(loss.collect())))
-            # print str(sum(loss.collect()))
-
-            # print loss.collect()
-            # print updates.collect()
             def getW(input):
                 if len(input) > 0:
                     return input[0]
@@ -160,11 +137,6 @@ def performUpdates(numIterations, V, W, H):
                 else: return (0,np.array([0]))
             WUpdates = updates.map(getW)
             HUpdates = updates.map(getH)
-            # print WUpdates.collect()
-            # print HUpdates.collect()
-            # print Woriginal.collect()
-            # print Woriginal.join(WUpdates).collect()
-            # print Horiginal.join(HUpdates).collect()
 
             # sum up np.array updates - ignore the "W" or "H" tag in values tuple
             def sumUpdates(inputValues):
@@ -178,11 +150,7 @@ def performUpdates(numIterations, V, W, H):
 
             Woriginal = Woriginal.mapValues(sumUpdates)
             Horiginal = Horiginal.mapValues(sumUpdates)
-            # print Woriginal.collect()
-            # print Horiginal.collect()
     return Woriginal, Horiginal
-            # print test.collect()
-            # print partitioned.mapPartitions(sgd).collect()
 
 
 
@@ -194,35 +162,25 @@ if os.path.isdir(inputVFilePath):
     Wrdd = initialiseWH(Vrdd)
     Hrdd = initialiseWH(Vrdd)
 
-    # rdd = rdd.flatMap(lambda x: x.split("|"))
-    # rdd = rdd.mapValues(lambda x: x)
-    # rdd.flatMapValues(lambda x: x.split("\n"))
-    # rdd = rdd.mapValues(lambda x: x.split("|"))
-    # rdd.partitionBy(rdd.keys().count(),rdd.values().map(lambda x: x[0]))
     print Vrdd.collect()
 
 elif os.path.isfile(inputVFilePath):
-    # lines = []
-    # for line in fileinput.input(inputVFilePath):
-    #     lines.append(line)
-    # rddMatrix = sc.parallelize([[1,2,3],[4,5,6],[7,8,9]])
     rddLines = sc.textFile(inputVFilePath)
-    # print rddLines.collect()
     rddLines = rddLines.map(lambda x: x.split(","))
-    # print rddLines.collect()
 
-    # row = np.array([])
-    # col = np.array([])
     Vrdd = rddLines.map(lambda x: ((int(x[0]) % numWorkers), (x[0], x[1], x[2])))
-    # print Vrdd.collect()
 
     # initialise W and H
     initialisedFactors = initialiseWH(Vrdd)
     Wrdd = initialisedFactors[0]
     Hrdd = initialisedFactors[1]
+
+    # performed dsgd - collect W and H
     updated = performUpdates(numIterations, Vrdd, Wrdd, Hrdd)
     factoredW = updated[0]
     factoredH = updated[1]
+
+    #unwrap W and H to output as CSV
     Wout = factoredW.map(lambda x: x[1][1:len(x[1])])
     Wout = np.asarray(Wout.collect())
     Wout = np.asarray([item for sublist in Wout for item in sublist])
@@ -231,26 +189,16 @@ elif os.path.isfile(inputVFilePath):
     Hout = factoredH.map(lambda x: x[1][1:len(x[1])])
     Hout = np.asarray(Hout.collect())
     Hout = np.asarray([item for sublist in Hout for item in sublist])
+    Hout = np.transpose(Hout)
     np.savetxt(Hfile, Hout, delimiter=",")
-    # Hout = factoredH.map(lambda x: x[1][1:len(x[1])])
-    # Hout = np.asarray(Hout.collect())
-    # np.savetxt(Hfile, Hout, delimiter=",")
-# print factoredW.collect()
-# print factoredH.collect()
 
 
-#
-# print Wrdd.collect()
-# print Hrdd.collect()
 
 # W, H, V, all RDDs
 # assign blocks as long as row and cols are independent, the set on a diagonal is a stratum
 # RDD for a strata.
 # partitionBy() function to define STRATA , mapPartitions() to operate on strata to give UPDATES to W and H via JOIN...
 
-
-# words = sc.parallelize(["scala","java","hadoop","spark","akka"])
-# print words.count()
 
 #sgd function returns list of tuples feed this to mapPartitions - return w,h = ([(row,np.array),()],[(col,np.array),()]) - then map
 #join
@@ -260,4 +208,3 @@ elif os.path.isfile(inputVFilePath):
 # sgd works on a block
 # map to get back to w and h
 
-# [[(4, array([ 31.29677002])), (4, array([ 14.50435528]))], [(7, array([ 38.29447413])), (5, array([ 12.87893311]))], [(1, array([ 4.30013615])), (7, array([ 1.97311161])), (1, array([ 2.72323718])), (7, array([ 4.30013869])), (1, array([ 1.97311195])), (7, array([ 2.72323709]))], [(2, array([ 6.79649267])), (8, array([ 10.26353537])), (2, array([ 6.17805152])), (8, array([ 16.65855151])), (2, array([ 6.79649235])), (8, array([ 10.26353544]))]]
